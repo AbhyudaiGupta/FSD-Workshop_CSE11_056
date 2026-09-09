991f4719cf6d3d30ce4ecfce52792d28dbdeec58
AbhyudaiGupta <abhyudaimain@gmail.com>
2026-09-09 22:06:05 +0530

    Assignment 3

--- Backend/Lab 3/httpserver1.js
+++ Backend/Lab 3/httpserver1.js
@@ -118,28 +118,104 @@ const server = http.createServer((req, resp) => {
         resp.end(JSON.stringify(users));
     }
 
-    //DELETE /delete/:id
-    else if(url === "/delete" && method === "DELETE") {
-        const id=url.split("/")[2];
-        const index = userData.findIndex(user => user.id === parseInt(id));
+
+    // PUT /edit/:id
+    else if (url.startsWith("/edit/") && method === "PUT") {
+
+        const id = parseInt(url.split("/")[2]);
+
+        let body = "";
+
+        // Receive data
+        req.on("data", (chunk) => {
+            body += chunk.toString();
+        });
+
+        // When data receiving is complete
+        req.on("end", () => {
+
+            try {
+
+                const data = JSON.parse(body);
+
+                // Find user
+                const index = users.findIndex(user => user.id === id);
+
+                // User not found
+                if (index === -1) {
+
+                    resp.writeHead(404, {
+                        "Content-Type": "application/json"
+                    });
+
+                    resp.end(JSON.stringify({
+                        message: "User not found"
+                    }));
+
+                    return;
+                }
+
+                // Update user
+                users[index] = {
+                    id: id,
+                    name: data.name,
+                    class: data.class
+                };
+
+                resp.writeHead(200, {
+                    "Content-Type": "application/json"
+                });
+
+                resp.end(JSON.stringify({
+                    message: "User updated successfully",
+                    user: users[index]
+                }));
+
+            } catch (error) {
+
+                resp.writeHead(400, {
+                    "Content-Type": "application/json"
+                });
+
+                resp.end(JSON.stringify({
+                    message: "Invalid JSON data"
+                }));
+            }
+        });
+    }
+
+
+    // DELETE /delete/:id
+    else if (url.startsWith("/delete/") && method === "DELETE") {
+
+        const id = parseInt(url.split("/")[2]);
+
+        const index = users.findIndex(user => user.id === id);
+
         if (index !== -1) {
-            userData.splice(index, 1);
+
+            users.splice(index, 1);
+
             resp.writeHead(200, {
                 "Content-Type": "application/json"
             });
+
             resp.end(JSON.stringify({
                 message: "User deleted successfully"
             }));
-        }
-        else {
+
+        } else {
+
             resp.writeHead(404, {
                 "Content-Type": "application/json"
             });
+
             resp.end(JSON.stringify({
                 message: "User not found"
             }));
         }
     }
+
 
     // Unknown route
     else {
